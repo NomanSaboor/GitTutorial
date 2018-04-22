0284315e00608160382660cb8ada832aa888b60f
Noman Saboor <noman.saboor1@gmail.com>
2018-04-22 15:25:08 +0500

    Revert.

--- GitTutorial/ViewController.swift
+++ GitTutorial/ViewController.swift
@@ -11,8 +11,6 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
-        print("Hello Nomi!")
-        print("Hello World!")
         super.viewDidLoad()
     }
 }
